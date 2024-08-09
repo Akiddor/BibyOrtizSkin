0a852f1849824fd6f5bedf32e18c28c7d0c75161
Akiddor <Akiddor7@gmail.com>
2024-08-08 18:46:35 -0600

    Nuevos Cambios

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleNewTreatment(treatmentId) {
+    var content = document.getElementById(treatmentId);
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}
